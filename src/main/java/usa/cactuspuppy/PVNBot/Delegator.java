86cfbe83f10b1f9ce799b63f40b52d4e144da232
CactusPuppy <coby.zhong@gmail.com>
2018-12-21 13:13:19 -0800

    More setup

---
+++ src/main/java/usa/cactuspuppy/PVNBot/Delegator.java
@@ -0,0 +1,6 @@
+package usa.cactuspuppy.PVNBot;
+
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class Delegator extends ListenerAdapter {
+}
---
+++ src/main/java/usa/cactuspuppy/PVNBot/Main.java
@@ -0,0 +1,13 @@
+package usa.cactuspuppy.PVNBot;
+
+import lombok.Getter;
+import net.dv8tion.jda.core.JDA;
+
+public class Main {
+    @Getter private static JDA invitationJDA;
+    @Getter private static JDA mainlogicJDA;
+
+    public static void main(String[] args) {
+
+    }
+}
